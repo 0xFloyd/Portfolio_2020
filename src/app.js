4f1f5b5076352214ab0d803de5f2604c5f50c275
Ryan Floyd <arfloyd7@gmail.com>
2020-07-09 14:21:46 -0400

    added joystick

--- src/app.js
+++ src/app.js
@@ -220,6 +220,106 @@ Ammo().then((Ammo) => {
     }
   }
 
+  const joystick = createJoystick(document.getElementById("wrapper"));
+
+  //setInterval(() => console.log(joystick.getPosition()), 1000);
+
+  function createJoystick(parent) {
+    const maxDiff = 100; //how far drag can go
+    const stick = document.createElement("div");
+    stick.classList.add("joystick");
+
+    stick.addEventListener("mousedown", handleMouseDown);
+    document.addEventListener("mousemove", handleMouseMove);
+    document.addEventListener("mouseup", handleMouseUp);
+    stick.addEventListener("touchstart", handleMouseDown);
+    document.addEventListener("touchmove", handleMouseMove);
+    document.addEventListener("touchend", handleMouseUp);
+
+    let dragStart = null;
+    let currentPos = { x: 0, y: 0 };
+
+    function handleMouseDown(event) {
+      stick.style.transition = "0s";
+
+      if (event.changedTouches) {
+        dragStart = {
+          x: event.changedTouches[0].clientX,
+          y: event.changedTouches[0].clientY,
+        };
+
+        return;
+      }
+      dragStart = {
+        x: event.clientX,
+        y: event.clientY,
+      };
+    }
+
+    function handleMouseMove(event) {
+      if (dragStart === null) return;
+      event.preventDefault();
+
+      //console.log("entered handleMouseMove");
+      if (event.changedTouches) {
+        event.clientX = event.changedTouches[0].clientX;
+        event.clientY = event.changedTouches[0].clientY;
+        //touchEvent(currentPos);
+      }
+
+      const xDiff = event.clientX - dragStart.x;
+      const yDiff = event.clientY - dragStart.y;
+      const angle = Math.atan2(yDiff, xDiff);
+      const distance = Math.min(maxDiff, Math.hypot(xDiff, yDiff));
+      const xNew = distance * Math.cos(angle);
+      const yNew = distance * Math.sin(angle);
+      stick.style.transform = `translate3d(${xNew}px, ${yNew}px, 0px)`;
+      currentPos = { x: xNew, y: yNew };
+      touchEvent(currentPos);
+    }
+
+    function handleMouseUp(event) {
+      if (dragStart === null) return;
+      stick.style.transition = ".2s";
+      stick.style.transform = `translate3d(0px, 0px, 0px)`;
+      dragStart = null;
+      currentPos = { x: 0, y: 0 };
+      moveDirection.forward = 0;
+      moveDirection.left = 0;
+      moveDirection.right = 0;
+      moveDirection.back = 0;
+    }
+
+    parent.appendChild(stick);
+    return {
+      getPosition: () => currentPos,
+    };
+  }
+
+  function touchEvent(coordinates) {
+    if (coordinates.x > 30) {
+      moveDirection.right = 1;
+      moveDirection.left = 0;
+    } else if (coordinates.x < -30) {
+      moveDirection.left = 1;
+      moveDirection.right = 0;
+    } else {
+      moveDirection.right = 0;
+      moveDirection.left = 0;
+    }
+
+    if (coordinates.y > 30) {
+      moveDirection.back = 1;
+      moveDirection.forward = 0;
+    } else if (coordinates.y < -30) {
+      moveDirection.forward = 1;
+      moveDirection.back = 0;
+    } else {
+      moveDirection.forward = 0;
+      moveDirection.back = 0;
+    }
+  }
+
   //create flat plane
   function createBlock() {
     // block properties
