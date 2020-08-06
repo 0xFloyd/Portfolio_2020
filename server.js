d6ef2028645eb5b8e65dcf084007dc82b9ff46b7
Ryan Floyd <arfloyd7@gmail.com>
2020-08-06 13:28:28 -0400

    added force SSL

--- server.js
+++ server.js
@@ -6,11 +6,20 @@ const app = express();
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 
-app.use(function (req, res) {
-  if (req.headers["x-forwarded-proto"] !== "https") {
-    return res.redirect("https://" + req.hostname + req.baseUrl);
+app.use(function (req, res, next) {
+  if (req.headers["x-forwarded-proto"] === "https") {
+    return next();
   }
+  res.redirect("https://" + req.hostname + req.baseUrl);
 });
+
+/*
+could be req.headers.host instead of req.hostname or req.url
+or req.get('hostname') or 'Host'
+
+For anyone using a newer version of Express (4.x) you will need to update the redirect to use baseUrl instead of url. The line 
+should read: return res.redirect(['https://', req.get('Host'), req.baseUrl].join('')); – n-devr Sep 27 '18 at 18:22
+*/
 
 // send the user to index html page inspite of the url
 app.get("*", (req, res) => {
