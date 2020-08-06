05f79e75e29293de0be62ad0a4b13c1c1d997c6c
Ryan Floyd <arfloyd7@gmail.com>
2020-08-06 12:53:27 -0400

    added force SSL

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.use(function (req, res, next) {
-  if (!req.secure || req.get("x-forwarded-proto") !== "https") {
-    return res.redirect("https://" + req.get("Host") + req.baseUrl);
+  if (req.headers["x-forwarded-proto"] !== "https") {
+    return res.redirect("https://" + req.hostname + req.url);
   }
   next();
 });
